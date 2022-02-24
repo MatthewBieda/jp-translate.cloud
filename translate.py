13cc77f7d551cf6a4b9c202dce0ed11dab8cc0a6
MatthewBieda <63665300+MatthewBieda@users.noreply.github.com>
2022-02-24 22:19:26 +0000

    Update translate.py

--- translate.py
+++ translate.py
@@ -14,19 +14,23 @@ import truecase
 st.set_page_config(page_title="jp-translate.io", page_icon="random")
 # Header
 st.title("jp-translate.io")
-print("page loaded")
 
 # Initialize tokenizers
 mt, md = MosesTokenizer(lang='en'), MosesDetokenizer(lang='en')
 tagger = fugashi.Tagger('-Owakati')
-print("tokenizers initalized")
 
 @st.cache(allow_output_mutation=True)
-def load_ENJP():
+def load_model():
         
-    ct_model_path = "ENJP_ctranslate2"
-    sp_source_model_path = "EN_Final.model"
-    sp_target_model_path = "JP_Final.model"
+    if option == "English-to-Japanese":
+        ct_model_path = "ENJP_ctranslate2"
+        sp_source_model_path = "EN_Final.model"
+        sp_target_model_path = "JP_Final.model"
+
+    if option == "Japanese-to-English":
+        ct_model_path = "JPEN_ctranslate2"
+        sp_source_model_path = "JP_Final.model"
+        sp_target_model_path = "EN_Final.model"
 
     # Create objects of CTranslate2 Translator and SentencePieceProcessor to load the models
     translator = ctranslate2.Translator(ct_model_path, "cpu")    # or "cuda" for GPU
@@ -34,21 +38,6 @@ def load_ENJP():
     sp_target_model = spm.SentencePieceProcessor(sp_target_model_path)
 
     return translator, sp_source_model, sp_target_model
-
-@st.cache(allow_output_mutation=True)
-def load_JPEN():
-
-    ct_model_path = "JPEN_ctranslate2"
-    sp_source_model_path = "JP_Final.model"
-    sp_target_model_path = "EN_Final.model"
-
-    # Create objects of CTranslate2 Translator and SentencePieceProcessor to load the models
-    translator = ctranslate2.Translator(ct_model_path, "cpu")    # or "cuda" for GPU
-    sp_source_model = spm.SentencePieceProcessor(sp_source_model_path)
-    sp_target_model = spm.SentencePieceProcessor(sp_target_model_path)
-
-    return translator, sp_source_model, sp_target_model
-
 
 def translate(source, translator, sp_source_model, sp_target_model):
     """Use CTranslate model to translate a sentence
@@ -63,34 +52,40 @@ def translate(source, translator, sp_source_model, sp_target_model):
     """
 
     if option == "English-to-Japanese":
-        print("translating English")
         source = source.lower()
-        source_sentences = sent_tokenize(source)
-        source_tokenized = sp_source_model.encode(source_sentences, out_type=str)
-        translations = translator.translate_batch(source_tokenized)
-        translations = [translation[0]["tokens"] for translation in translations]
-        translations_detokenized = sp_target_model.decode(translations)
-        translation = " ".join(translations_detokenized)
-        normalized = neologdn.normalize(translation)
-        return normalized
+        newarray = []
+        splitsource = source.splitlines()
+        for entry in splitsource:
+            source_sentences = sent_tokenize(entry)
+            source_tokenized = sp_source_model.encode(source_sentences, out_type=str)
+            translations = [translation[0]["tokens"] for translation in translations]
+            translations_detokenized = sp_target_model.decode(translations)
+            translation = " ".join(translations_detokenized)
+            normalized = neologdn.normalize(translation)
+            newarray.append(normalized)
+        final = "\n".join(newarray)
+        return final
 
     if option == "Japanese-to-English":
-        print("translating Japanese")
         # use regex to split input sentences on fullstop (keeping it) into a list
         source = re.split(r'(?<=\。)', source)
         newlist = []
         # optimal to use mecab to parse sentence by sentence
         for sentence in source:
             source_sentences = tagger.parse(sentence)
-            newlist.append(source_sentences)
-        source_tokenized = sp_source_model.encode(newlist, out_type=str)
-        translations = translator.translate_batch(source_tokenized)
-        translations = [translation[0]["tokens"] for translation in translations]
-        translations_detokenized = sp_target_model.decode(translations)
-        mosesdetok = md.detokenize(translations_detokenized)
-        truecased = truecase.get_true_case(mosesdetok)
-        return truecased
-
+            sourceaslist = [source_sentences]
+            source_tokenized = sp_source_model.encode(sourceaslist, out_type=str)
+            translations = translator.translate_batch(source_tokenized)
+            translations = [translation[0]["tokens"] for translation in translations]
+            translations_detokenized = sp_target_model.decode(translations)
+            mosesdetok = md.detokenize(translations_detokenized)
+            truecased = truecase.get_true_case(mosesdetok)
+            if '\n' in sentence:
+                newlist.append('\n' + truecased)
+            else:
+                newlist.append(truecased)
+        final = "\n".join(newlist)
+        return final
 
 # Form to add your items
 with st.form("my_form"):
@@ -99,16 +94,12 @@ with st.form("my_form"):
     option = st.selectbox(
     "Select Language Pair",
     ("English-to-Japanese", "Japanese-to-English"))
-    print("language selected")
 
     # Textarea to type the source text.
     user_input = st.text_area("Source Text", max_chars=2000)
 
     # Load models
-    if option == "English-to-Japanese":
-        translator, sp_source_model, sp_target_model = load_ENJP()
-    elif option == "Japanese-to-English":
-        translator, sp_source_model, sp_target_model = load_JPEN()
+    translator, sp_source_model, sp_target_model = load_model()
     
     # Translate with CTranslate2 model
     translation = translate(user_input, translator, sp_source_model, sp_target_model)
@@ -121,12 +112,14 @@ with st.form("my_form"):
         st.write("Translation")
         st.info(translation)
 
-st.markdown('Interested in how this was built? Read the research paper :book:') 
+st.markdown('Interested in how this was built? [Read the research paper](https://arxiv.org/abs/2202.11669) :book:') 
 
-with open('Research.pdf',"rb") as f:
-   st.download_button('Download paper', f, "Research.pdf")
+st.markdown('Interested in improving this project? [Contact me](https://matthewbieda.github.io/)')
 
-st.markdown('Interested in improving this project? [Contact me](https://matthewbieda.github.io/)') 
+st.text(" ")
+
+st.markdown("<p style='text-align: center;'>Created by Matthew Bieda. Assisted by Yasmin Moslem.</p>", unsafe_allow_html=True)
+ 
 
 # Optional Style (courtesy of ymoslem)
 st.markdown(""" <style>
